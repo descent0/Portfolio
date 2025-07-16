9a79f501e2ee93f7d3a778d10ffaec8dfefa934d
decent0 <rajputdishant891@gmail.com>
2025-07-17 02:46:55 +0530

    issue of ui resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,24 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Portfolio',
+  description: 'Portfolio of Dishant Rajput',
+  generator: 'Portfolio',
   icons: {
     icon: '/fav1.png',
+  },
+  openGraph: {
+    title: 'Portfolio',
+    description: 'Portfolio of Dishant Rajput',
+    images: [
+      {
+        url: '/portfolio.png',
+        width: 1200,
+        height: 630,
+        alt: 'Portfolio',
+      },
+    ],
+    type: 'website',
   },
   
 }
