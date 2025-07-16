b2d38fe399a4c860e0875268f461906e6ae50b7c
decent0 <rajputdishant891@gmail.com>
2025-07-17 01:58:23 +0530

    cross origin resolved

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   experimental: {
-    allowedDevOrigins: ['http://172.24.34.245:3000'], 
-  },
+ 
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint during production builds
   },
